043dbfeff4c8cda49101bc2a664fb268f9a0ec15
cumboandrea <cumboandrea@gmail.com>
2022-10-24 23:03:02 +0200

    Ultimo cambio componente Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,6 +7,12 @@ const Login = () => {
             <div className="row py-5">
                 <div className="col-12">
                     <h1>Login</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, delectus dicta illo cumque libero perspiciatis voluptate est blanditiis repudiandae voluptatum?</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    Otra Linea
                 </div>
             </div>
         </div>
