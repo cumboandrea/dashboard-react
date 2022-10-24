7d2036798c31688a3beaa63919be6a2c4d280217
Andrea <cumboandrea@gmail.com>
2022-10-24 22:30:33 +0200

    Modifico component Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <div className="container">
+            <div className="row py-5">
+                <div className="col-12">
+                    <h1>Login</h1>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Login
